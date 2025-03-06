034384d079f0382ae1fab1dfbe515aa05f750420
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2025-03-06 15:15:24 +0200

    Fixed profile menu entry

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -90,8 +90,9 @@ const AppNavigation = () => {
                         <div className="relative">
                             <button className="flex items-center text-gray-400 hover:text-red-600 transition-all duration-300"
                                     onClick={() => setDropdownOpen(!dropdownOpen)}>
-                                <Image height={100} width={100} src={user.photoURL}
-                                     alt="Profile" className={`w-10 h-10 rounded-full ${user.photoURL&&"border-2"} border-gray-600 shadow-lg`} />
+                                {user.photoURL?<Image height={100} width={100} src={user.photoURL}
+                                        alt="Profile"
+                                        className={`w-10 h-10 rounded-full ${user.photoURL && "border-2"} border-gray-600 shadow-lg`}/>:<span className="block md:inline text-gray-400 hover:text-red-600 transition-all duration-300 py-2">Profile</span>}
                                 {unreadNotificationsCount > 0 && (
                                     <span className="absolute top-0 right-0 bg-red-600 text-white text-xs font-bold rounded-full px-1 shadow-lg">
                                         {unreadNotificationsCount}
